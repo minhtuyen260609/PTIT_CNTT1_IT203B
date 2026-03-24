2adabccd7538ea82e6e260a7b20581732d37d8eb
minhtuyen260609 <tuyendo260906@gmail.com>
2026-03-24 15:37:42 +0700

    Update Main.java

--- SS12/Bai3/Main.java
+++ SS12/Bai3/Main.java
@@ -2,26 +2,19 @@ package Bai3;
 
 public class Main {
     //Lỗi hiện tại
-    //
     //Code gọi Stored Procedure nhưng:
-    //
     //Chưa đăng ký tham số OUT
     //JDBC không biết kiểu dữ liệu trả về
-    //→ Lỗi column index is out of range hoặc không lấy được giá trị.
-    //✅ Vì sao phải registerOutParameter()?
+    //Vì sao phải registerOutParameter()?
     //Tham số OUT là dữ liệu DB trả về
     //JDBC bắt buộc phải:
     //Khai báo vị trí
     //Khai báo kiểu dữ liệu
-    //→ Mới đọc được kết quả sau execute()
 
 // CallableStatement cstmt = conn.prepareCall("{call GET_SURGERY_FEE(?, ?)}");
-//
 //cstmt.setInt(1, 505);
 //cstmt.registerOutParameter(2, Types.DECIMAL);
-//
 //cstmt.execute();
-//
 //double cost = cstmt.getDouble(2);
 //System.out.println(cost);
 }
