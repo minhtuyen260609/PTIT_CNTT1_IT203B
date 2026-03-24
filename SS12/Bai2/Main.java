b77b300f3bee8a6be546a66809d8747626762363
minhtuyen260609 <tuyendo260906@gmail.com>
2026-03-24 15:36:49 +0700

    Refactor Main.java by removing commented code

--- SS12/Bai2/Main.java
+++ SS12/Bai2/Main.java
@@ -2,18 +2,9 @@ package Bai2;
 
 public class Main {
 
-//Lỗi hiện tại
-//Nối chuỗi số vào SQL:
-//"temperature = " + temp
-//Máy dùng Locale khác nhau
-//Có nơi dùng 37.5, có nơi dùng 37,5
-//SQL chỉ chấp nhận dấu .
-//Dễ lỗi cú pháp.
 //Vì sao setDouble() / setInt() giải quyết được?
 //Không nối chuỗi thủ công
 //Giá trị số được truyền đúng kiểu dữ liệu
-//JDBC tự chuyển sang định dạng chuẩn của DB
-//Không phụ thuộc dấu chấm / dấu phẩy hệ điều hành
 
 // String sql = "UPDATE Vitals SET temperature = ?, heart_rate = ? WHERE p_id = ?";
 //PreparedStatement ps = conn.prepareStatement(sql);
