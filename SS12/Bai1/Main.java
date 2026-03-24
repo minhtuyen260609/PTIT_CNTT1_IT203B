bb4ba31b9c46a006511a6acfeb5ba6c0d2df7d28
minhtuyen260609 <tuyendo260906@gmail.com>
2026-03-24 15:35:41 +0700

    Update Main.java

--- SS12/Bai1/Main.java
+++ SS12/Bai1/Main.java
@@ -1,16 +1,13 @@
 package Bai1;
 
 //Lỗi SQL Injection
-//Code nối chuỗi trực tiếp dữ liệu người dùng vào câu SQL → kẻ xấu chèn thêm điều kiện luôn đúng:
+//Code nối chuỗi trực tiếp dữ liệu người dùng vào câu SQL 
 //Ví dụ nhập mật khẩu:
-//' OR '1'='1
-//→ Đăng nhập trái phép.
+//OR 1=1
+//Đăng nhập trái phép.
 // Vì sao PreparedStatement an toàn?
 //SQL và dữ liệu tách riêng
-//Dấu ? là tham số, không phải cú pháp SQL
-//DB biên dịch sẵn câu lệnh (pre-compiled)
-//Dữ liệu nhập vào chỉ được coi là giá trị
-// Không thể chèn lệnh SQL độc hại.
+//Dấu ? là tham số không phải cú pháp SQL
 
 //String sql = "SELECT * FROM Doctors WHERE code = ? AND pass = ?";
 //PreparedStatement ps = conn.prepareStatement(sql);
